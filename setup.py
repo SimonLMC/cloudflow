7d9ccb9fdef8936d888e4bf48685f36755b7d75c
Simon Le Mouellic <s.lemouellic@quinten-france.com>
2023-02-12 18:39:04 +0100

    package requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
-import setuptools
+import sys
+
+from pkg_resources import VersionConflict, require
+from setuptools import setup
+from pathlib import Path
+
+try:
+    require("setuptools>=38.3")
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
 
 
-setuptools.setup(
-    name='cloudflow',
-    version='0.0.2',
-    author='Simon Le Mouellic',
-    author_email='lemouellicsimon@gmail.com',
-    description='Testing installation of Package',
-    long_description_content_type="text/markdown",
-    url='https://github.com/SimonLMC/cloudflow',
-    license='MIT',
-    packages=['cloudflow'],
-    install_requires=[],
-)
+def read_requirements(basename):
+    requirements_file = Path(__file__).parent.absolute() / basename
+    with open(requirements_file) as f:
+        return [req.strip() for req in f.readlines()]
+
+
+if __name__ == "__main__":
+    requirements = read_requirements("requirements.txt")
+    setup(
+        use_pyscaffold=True,
+        install_requires=requirements
+    )
